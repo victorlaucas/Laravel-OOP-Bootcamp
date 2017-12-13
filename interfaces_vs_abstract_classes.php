dcc89029957534eb928d7bca6fe6988abd58ae05
Victor <victorlaucas@gmail.com>
2017-12-13 12:54:49 -0700

    Pushed up previous code without saving comment.... Reference this instead

--- interfaces_vs_abstract_classes.php
+++ interfaces_vs_abstract_classes.php
@@ -1,5 +1,6 @@
 <?php 
 
+// An interface can only define public methods. 
 interface Provider {
   public function authorize();
 }
@@ -8,3 +9,21 @@ function login(Provider $provider)
 {
   $provider->authorize();
 }
+
+// An abstract class cannot be instantiated on its own 
+// Can create abstract protected methods 
+abstract class Provider {
+  abstract protected function getAuthorizationUrl();
+}
+// requires that a subclass creates and implements method above
+
+class FacebookProvider extends Provider {
+  protected function getAuthorizationUrl()
+  {
+    return '';
+  }
+}
+
+// An interface defines a public api. A contract that any implementation has to obey. No logic will be stored in an interface. 
+
+// An abstract class can enforce a contract by creating abstract methods and that way any subclass must obey. Must also resort to inheritance which isn't always the best idea. 
